4f497ec0888f66274cbe0b426efbbda1670b8e97
islandkbm <islandkbm@gmail.com>
2022-11-28 18:11:13 +0900

    local

--- frontend/myappf/src/App.js
+++ frontend/myappf/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import myAppGlobal from "./myAppGlobal";
 
 function App() {
   console.log("-------------------------react APP start---------------------");
-  console.log("Hostname : " +window.location.hostname + ",host : " + window.location.host + ",Platform : " + window.platform );
+  console.log("Hostname : " +window.location.hostname + ",host : " + window.location.protocol);
   
+
+  if (window.location.hostname.indexOf("amazonaws.com") != -1  || window.location.hostname.indexOf("13.209.26.2") != -1 ) {
+//서버 IP이거나 도메인이 서버이면 서버접속임.
+    myAppGlobal.islocal = false;
+    myAppGlobal.isuseradmin = false;
+    console.log("-------------------------connected aws server---------------------");
+    
+  } else {
+    ///로컬로 접속하면 관리자 계정임
+    myAppGlobal.islocal = true;
+    myAppGlobal.isuseradmin = true;
+    console.log("-------------------------connected local network---------------------");
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ frontend/myappf/src/myAppGlobal.js
@@ -0,0 +1,13 @@
+
+
+
+var myAppGlobal = {
+    islogin : false, // 
+    issupervisor:false, //공장설정 가능 슈퍼바이저 
+    islocal: false,  // 로컬, 원격
+    isuseradmin: false, // 사용자 관리자계정(장비설정가능) 나머지 그냥 뷰어
+    farmapi: undefined,
+    ncount:0
+  }
+  
+export default myAppGlobal;
