118a988ae933457d9237263af6a6e7124e95240a
islandkbm <islandkbm@gmail.com>
2022-12-05 18:26:16 +0900

    Update sensorpage.js

--- frontend/myappf/src/pages/sensorpage.js
+++ frontend/myappf/src/pages/sensorpage.js
@@ -3,46 +3,30 @@ import Sensordisplay from "../sensordisplay";
 import IndoorFarmAPI from "../indoorfarmapi";
 import myAppGlobal from "../myAppGlobal";
 
-
-
-
 const Sensorpage = () => {
+  const [msensorsarray, setSensors] = useState([]);
 
+  //console.log("-------------------------Sensorpage  ---------------------");
 
-    const [msensorsarray, setSensors] = useState([]);
-  
-    //console.log("-------------------------Sensorpage  ---------------------");
+  useEffect(() => {
+    let interval = null;
 
-    useEffect(() => {
-      let interval = null;
+    //aws 접속이면 5초에 한번만 읽자 머니 나가니까.
+    let readtimemsec = 1000;
+    if (myAppGlobal.islocal == false) {
+      readtimemsec = 5000;
+    }
+    interval = setInterval(() => {
+      myAppGlobal.farmapi.getsensors(myAppGlobal.islocal).then((ret) => {
+        let sensors = ret.retMessage;
+        setSensors(sensors);
+      });
+    }, readtimemsec);
 
-      //aws 접속이면 5초에 한번만 읽자 머니 나가니까.
-      let readtimemsec=1000;
-      if(myAppGlobal.islocal == false)
-      {
-        readtimemsec=5000;
-      }
-      interval = setInterval(() => {
-        myAppGlobal.farmapi.getsensors(myAppGlobal.islocal).then((ret) => {
-          let sensors = ret.retMessage;
-          setSensors(sensors);
-         });
-       
-      }, readtimemsec);
-  
-      return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    
-    return(
-        
-            <div>
-              {Sensordisplay(msensorsarray,true)}
-
-              
-              </div>
-        
-    )
-}
+  return <div>{Sensordisplay(msensorsarray, true)}</div>;
+};
 
 export default Sensorpage;
