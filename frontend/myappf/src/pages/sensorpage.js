4d5d4db2f66a4744f7f7b6941bffe146917c1508
islandkbm <islandkbm@gmail.com>
2022-12-05 18:25:52 +0900

    Update sensorpage.js

--- frontend/myappf/src/pages/sensorpage.js
+++ frontend/myappf/src/pages/sensorpage.js
@@ -11,27 +11,22 @@ const Sensorpage = () => {
 
     const [msensorsarray, setSensors] = useState([]);
   
-    console.log("-------------------------Sensorpage  ---------------------");
+    //console.log("-------------------------Sensorpage  ---------------------");
 
     useEffect(() => {
       let interval = null;
 
+      //aws 접속이면 5초에 한번만 읽자 머니 나가니까.
       let readtimemsec=1000;
       if(myAppGlobal.islocal == false)
       {
         readtimemsec=5000;
       }
-  
-      console.log("-------------------------Sensorpage useEffect ---------------------");
-
       interval = setInterval(() => {
-
         myAppGlobal.farmapi.getsensors(myAppGlobal.islocal).then((ret) => {
-          
           let sensors = ret.retMessage;
           setSensors(sensors);
          });
-
        
       }, readtimemsec);
   
