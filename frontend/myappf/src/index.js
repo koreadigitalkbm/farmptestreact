af0e3b721e5dce852cd42bdec2f75b70172dcc2e
islandkbm <islandkbm@gmail.com>
2023-01-10 10:21:13 +0900

    추가머지

--- frontend/myappf/src/index.js
+++ frontend/myappf/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import './lang/i18n';
 import './index.css';
 // import App from './App';
 import FarmApp from './FarmApp';
 import manistore from "./mainStore";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ frontend/myappf/src/lang/i18n.js
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import langEn from './lang.en.json';
+import langKo from './lang.ko.json';
+
+const resources = {
+    'en-US': {
+        translations: langEn
+    },
+    'ko-KR': {
+        translations: langKo
+    }
+}
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: resources,
+        lng: 'en-US',
+        debug: true,
+        defaultNS: 'translations',
+        ns: 'translations',
+        keySepartor: false,
+        interpolation: {
+            escapeValue: false
+        },
+        react: {
+            useSuspense: false
+        }
+    })
+
+export default i18n;
