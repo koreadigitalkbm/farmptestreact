c307adf425e814c0565b6b02bdd2d218ab93be6c
islandkbm <islandkbm@gmail.com>
2023-01-19 11:45:34 +0900

    Update FarmApp.js

--- frontend/myappf/src/FarmApp.js
+++ frontend/myappf/src/FarmApp.js
@@ -18,7 +18,7 @@ function FarmApp(props) {
     console.log("Hostname : " + window.location.hostname + ",host : " + window.location.protocol);
 
     myAppGlobal.isinitalizeApp = true;
-    if (window.location.hostname.indexOf("amazonaws.com") != -1 || window.location.hostname.indexOf("13.209.26.2") != -1) {
+    if (window.location.hostname.indexOf("amazonaws.com") != -1 || window.location.hostname.indexOf("52.79.226.255") != -1) {
       //서버 IP이거나 도메인이 서버이면 서버접속임.
       myAppGlobal.islocal = false;
       window.sessionStorage.setItem("islocal", false);
