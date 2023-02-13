9e901eca990b27d106761a317ebcc0c5f1ce4ff4
lmhjung <lmhjung@naver.com>
2023-02-13 11:40:50 +0900

    aaa

--- backend/localonly/camerainterface.js
+++ backend/localonly/camerainterface.js
@@ -3,6 +3,15 @@ const KDDefine = require("../../frontend/myappf/src/commonjs/kddefine");
 const KDCommon = require("../kdcommon");
 
 
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'photo',
+  output: '../../../common/ctestimage.jpg',
+  width: 640,
+  height: 480,
+  nopreview: true,
+});
+
 
 
 module.exports = class CameraInterface {
@@ -12,7 +21,18 @@ module.exports = class CameraInterface {
   // 사진을 촬용하고  bin 데이터를 base64 인코딩해서 리턴함. ( 서버로  http 로 보내야함으로)
   static Captureimage()
   {
-    return KDCommon.ReadfileBase64("../common/ctestimage.jpg");
+
+    myCamera.snap()
+      .then((result) => {
+        return KDCommon.ReadfileBase64("../common/ctestimage.jpg");
+        // Your picture was captured
+      })
+      .catch((error) => {
+        // Handle your error
+        console.bgMagenta('       ', 'Captureimage()', err)
+        return null;
+      });
+
   }
 
 };
