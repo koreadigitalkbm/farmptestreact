b9d1f1dc0477690ac6b44d3ef7b428138b7bbcd1
islandkbm <islandkbm@gmail.com>
2023-02-13 21:34:57 +0900

    Update camerainterface.js
    카메라  win32 플랫폼이 아닐경우 pi camera 적용

--- backend/localonly/camerainterface.js
+++ backend/localonly/camerainterface.js
@@ -2,37 +2,36 @@
 const KDDefine = require("../../frontend/myappf/src/commonjs/kddefine");
 const KDCommon = require("../kdcommon");
 
-
-const PiCamera = require('pi-camera');
-const myCamera = new PiCamera({
-  mode: 'photo',
-  output: '../../../common/ctestimage.jpg',
-  width: 640,
-  height: 480,
-  nopreview: true,
-});
-
-
+const os = require("os");
 
 module.exports = class CameraInterface {
-  constructor() {
-
-  }
+  constructor() {}
   // 사진을 촬용하고  bin 데이터를 base64 인코딩해서 리턴함. ( 서버로  http 로 보내야함으로)
-  static Captureimage()
-  {
-
-    myCamera.snap()
-      .then((result) => {
-        return KDCommon.ReadfileBase64("../common/ctestimage.jpg");
-        // Your picture was captured
-      })
-      .catch((error) => {
-        // Handle your error
-        console.bgMagenta('       ', 'Captureimage()', err)
-        return null;
+  static Captureimage() {
+    if (os.platform() == "win32") {
+      return KDCommon.ReadfileBase64("../common/ctestimage.jpg");
+      
+    } else {
+      const PiCamera = require("pi-camera");
+      const myCamera = new PiCamera({
+        mode: "photo",
+        output: "../../../common/ctestimage.jpg",
+        width: 640,
+        height: 480,
+        nopreview: true,
       });
 
+      myCamera
+        .snap()
+        .then((result) => {
+          return KDCommon.ReadfileBase64("../common/ctestimage.jpg");
+          // Your picture was captured
+        })
+        .catch((error) => {
+          // Handle your error
+          console.bgMagenta("       ", "Captureimage()", err);
+          return null;
+        });
+    }
   }
-
 };
