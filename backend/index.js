94f7057e39a835ec5c92451306a32e2f86b6ae0b
islandkbm <islandkbm@gmail.com>
2023-02-17 15:23:49 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,21 +28,14 @@ mMainclass.mAPI.firebasedbsetup();
 
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 app.use(express.static("./backend/"));
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.use(express.urlencoded({extended: true})); 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use( bodyParser.json({limit: '50mb'}) );
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: true,
-  parameterLimit:50000
-}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+
+
 
 //서버에 요청
 app.use("/api/farmrequest", function (req, res) {
