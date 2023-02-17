e888b854bfe957e56ce615e4ba27007143adc1a1
islandkbm <islandkbm@gmail.com>
2023-02-17 15:01:56 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("./backend/"));
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 
 
 //서버에 요청
