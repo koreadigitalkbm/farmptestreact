a5e244212ded663b67ef5506203b90461a448188
islandkbm <islandkbm@gmail.com>
2023-02-17 14:22:55 +0900

    Update cmoscamera.js

--- backend/localonly/rasberrypi/cmoscamera.js
+++ backend/localonly/rasberrypi/cmoscamera.js
@@ -27,11 +27,14 @@ module.exports = class RasCamera {
         });
         console.log('======================= 1 start pi-camera', '/home/pi/kd/farmptestreact/common/ctestimage2.jpg')
   
-        return await myCamera.snap()
+        return await myCamera.snapDataUrl()
           .then((data) => {    // data << 이미지 데이타가 아님... 
-            let data_img = KDCommon.ReadfileBase64( '/home/pi/kd/farmptestreact/common/ctestimage2.jpg' );
-            console.log(" data : " + data_img.length);
-            return data_img;
+            //let data_img = KDCommon.ReadfileBase64( '/home/pi/kd/farmptestreact/common/ctestimage2.jpg' );
+            //console.log(" data : " + data_img.length);
+            
+            //"data:image/jpg;base64," 문자열 제거
+            
+            return  data.substring(22);
    
           })
           .catch((error) => {
