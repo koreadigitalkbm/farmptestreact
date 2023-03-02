0a2b00ce6105351c35151dd65a37f8428f93c2b6
islandkbm <islandkbm@gmail.com>
2023-03-02 19:02:55 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@
 //1.815 : 카메라 촬영 버그 수정
 //1.816 : 자동제어 Number() 적용
 //1.817 : 프론트엔드  UI 개선
+//2.101 : 프로그램 업데이트기능 추가  1자리수 변경시 백엔드 npm install 실행,  소수점 첫번째 자리 숫자 변경시  frontend  install 실행, 
 const farmscubeplatformversion = 2.101;
 
 var express = require("express");
