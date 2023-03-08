16eb77760605c3de56fa2e657d4b3e4242674283
islandkbm <islandkbm@gmail.com>
2023-03-08 16:17:18 +0900

    Update serverapi.js

--- backend/serveronly/serverapi.js
+++ backend/serveronly/serverapi.js
@@ -122,11 +122,11 @@ module.exports = class ServerAPI {
 
       let objJsonB64encode = Buffer.from(jsonstr).toString("base64");
 
-      
+      repskey.set("");
       reqkey.set(objJsonB64encode);
 
       // 이벤트 리스너 한번만
-      repskey.on("value", (snapshot) => {
+      repskey.once("value", (snapshot) => {
         const repsdata = snapshot.val();
         //        console.log(repsdata);
 
