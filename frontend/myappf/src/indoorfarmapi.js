f41ee1c95a68de8868034abddae50cfd0082d063
islandkbm <islandkbm@gmail.com>
2023-08-17 12:25:17 +0900

    Update indoorfarmapi.js

--- frontend/myappf/src/indoorfarmapi.js
+++ frontend/myappf/src/indoorfarmapi.js
@@ -24,7 +24,7 @@ export default class IndoorFarmAPI {
         "Session-ID": myAppGlobal.sessionid,
       },
       body: JSON.stringify(data), //
-      signal: AbortSignal.timeout(40000) //40초타임아웃설정
+      signal: AbortSignal.timeout(300000) //300초타임아웃설정
     });
     return await response.json();
   }
