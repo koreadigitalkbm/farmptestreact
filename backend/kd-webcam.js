03faa79b8cddd0b1a1c523758ae725ef12d72bef
islandkbm <islandkbm@gmail.com>
2023-08-21 18:21:13 +0900

    Update kd-webcam.js

--- backend/kd-webcam.js
+++ backend/kd-webcam.js
@@ -22,14 +22,14 @@ const moment = require('moment');
 
 let shell = require('shelljs');
 var schedule = require('node-schedule');
-
+//scp usbcamimage.jpg root@127.0.0.1:"C:\user\abc"
 // ============================================================================= job pm2 flush
 // ==========        sudo pm2 flush
 // ============================================================================= job pm2 flush
 //let Job_Get_Image = new schedule.scheduleJob( '0 */1 * * * *' , function () { 
     try {
 //        console.log( '   '.bgMagenta, 'now run pm2 flush command' );
-        let _cmd = `fswebcam -r 1280*960 --no-banner /home/pi/kd/farmptestreact/common/usbcamimage.jpg`
+        let _cmd = `fswebcam -r 1280*960 --no-banner /home/pi/kd/farmptestreact/frontend/myappf/public/usbcamimage.jpg`
         if (shell.exec(`${ _cmd }`).code === 0) {
             console.log('done !!! get image',  );
         }
