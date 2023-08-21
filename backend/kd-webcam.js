1a9b701489698ba1f3fdb84cceff345f6dc8ce25
islandkbm <islandkbm@gmail.com>
2023-08-21 18:55:07 +0900

    Update kd-webcam.js

--- backend/kd-webcam.js
+++ backend/kd-webcam.js
@@ -45,7 +45,7 @@ const PiCamera = require("pi-camera");
        
         console.log('======================= 1 start pi-camera', '/home/pi/kd/farmptestreact/common/ctestimage2.jpg')
   
-          myCamera.snapDataUrl()
+          myCamera.snap()
           .then((data) => {    // data << 이미지 데이타가 아님... 
             //let data_img = KDCommon.ReadfileBase64( '/home/pi/kd/farmptestreact/common/ctestimage2.jpg' );
             //console.log(" data : " + data_img.length);
