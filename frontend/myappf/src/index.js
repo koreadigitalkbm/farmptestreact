5da7d39826d32d9577caf94e91aa4b63c475da88
islandkbm <islandkbm@gmail.com>
2025-01-21 18:16:48 +0900

    Update index.js

--- frontend/myappf/src/index.js
+++ frontend/myappf/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import './index.css';
 import FarmApp from './FarmApp';
@@ -9,11 +9,27 @@ import { CookiesProvider } from 'react-cookie';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function HTMLPage() {
+  return (
+    <iframe
+      src="http://localhost:8877/dataget/jbuchamber.html" // public/sample.html로 경로 지정
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Sample HTML Page"
+    ></iframe>
+  );
+}
+
 root.render(
   <Provider store = {manistore}>
     <CookiesProvider> 
      <BrowserRouter>
-    <FarmApp />
+     <Routes>
+        <Route path="/datagetjbuniv" element={<HTMLPage />} />
+        <Route path="/" element={<FarmApp />} />
+      </Routes>
+
+    
     </BrowserRouter>
     </CookiesProvider> 
   </Provider>
